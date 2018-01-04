5c5f9cab0af5147335e540b9314ccd1eaa198f69
FotonCheg <kvanta2009@meta.ua>
2018-01-04 22:35:50 +0200

    DB worked

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,19 @@
 class PagesController < ApplicationController
   def welcome
     @first_var = 'Hi my dear friend, it is Ruby tutorial'
+    Article.create(title:"Title", body:"My long body of article")#Создали объект статьи и сразу же сохранили его с данными в БД
+    @article_from_db = Article.all #Достали все статьи из БД и записали в переменную
+
+    #Бежим по всем статьям и достаем из них данные
+    @article_from_db.each do |elem|
+      @first_var + elem.title + " " + elem.body #почему-то не добавляет данные в переменную. Я что-то читал про то, что переменные внутри блока могут странно себя вести
+      puts elem.title + " " + elem.body
+    end
+  end
+
+  #Это метод, который доступен по пути /create_new_article и если мы передаем ему параметры http://localhost:3000/create_new_articles?title=Yaroslav&body=Dyachenko, то они пишутся в бд
+  def create_new_article
+    Article.create(title:params[:title], body:params[:body])
   end
 end
 
